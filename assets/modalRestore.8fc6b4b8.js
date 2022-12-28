6b7fb094baad62a72e70d2cf850145de0a9be9eb
luohc92 <luohc92@gmail.com>
2022-12-28 15:19:43 +0800

    test

---
+++ assets/modalRestore.8fc6b4b8.js
@@ -0,0 +1,117 @@
+import{d as Z,r as x,a as K,E as uu,c as eu,b as V,e as s,w as i,o as nu,t as T,f as N,n as tu}from"./index.4cdc9353.js";import{c as su,d as lu,b as ou,E as au,a as iu,e as ru,f as cu,g as fu,h as Eu,_ as z,i as k}from"./mirrorEditor.vue_vue_type_style_index_0_lang.52a14628.js";import{j as pu}from"./index.0bb074ad.js";const du={class:"tool-head"},Fu={class:"value"},mu={class:"value"},gu=N("\u683C\u5F0F\u5316"),hu=N("\u8F6C\u6362"),_u={class:"right"},Au=N("\u590D\u5236"),Cu=Z({__name:"modalRestore",setup(bu){const{toClipboard:M}=su(),b=x(`{
+    "BigIntSupported": 995815895020119800000,
+    "date": "20180322",
+    "message": "Success !",
+    "status": 200,
+    "city": "\u5317\u4EAC",
+    "count": 632,
+    "data": {
+        "shidu": "34%",
+        "pm25": 73,
+        "pm10": 91,
+        "quality": "\u826F",
+        "wendu": "5",
+        "ganmao": "\u6781\u5C11\u6570\u654F\u611F\u4EBA\u7FA4\u5E94\u51CF\u5C11\u6237\u5916\u6D3B\u52A8",
+        "yesterday": {
+            "date": "21\u65E5\u661F\u671F\u4E09",
+            "sunrise": "06:19",
+            "high": "\u9AD8\u6E29 11.0\u2103",
+            "low": "\u4F4E\u6E29 1.0\u2103",
+            "sunset": "18:26",
+            "aqi": 85,
+            "fx": "\u5357\u98CE",
+            "fl": "<3\u7EA7",
+            "type": "\u591A\u4E91",
+            "notice": "\u9634\u6674\u4E4B\u95F4\uFF0C\u8C28\u9632\u7D2B\u5916\u7EBF\u4FB5\u6270",
+            "yesterday": {
+                "date": "21\u65E5\u661F\u671F\u4E09",
+                "sunrise": "06:19",
+                "high": "\u9AD8\u6E29 11.0\u2103",
+                "low": "\u4F4E\u6E29 1.0\u2103",
+                "sunset": "18:26",
+                "aqi": 85,
+                "fx": "\u5357\u98CE",
+                "fl": "<3\u7EA7",
+                "type": "\u591A\u4E91",
+                "notice": "\u9634\u6674\u4E4B\u95F4\uFF0C\u8C28\u9632\u7D2B\u5916\u7EBF\u4FB5\u6270"
+            }
+        },
+        "forecast": [
+            {
+                "date": "22\u65E5\u661F\u671F\u56DB",
+                "sunrise": "06:17",
+                "high": "\u9AD8\u6E29 17.0\u2103",
+                "low": "\u4F4E\u6E29 1.0\u2103",
+                "sunset": "18:27",
+                "aqi": 98,
+                "fx": "\u897F\u5357\u98CE",
+                "fl": "<3\u7EA7",
+                "type": "\u6674",
+                "notice": "\u613F\u4F60\u62E5\u6709\u6BD4\u9633\u5149\u660E\u5A9A\u7684\u5FC3\u60C5"
+            },
+            {
+                "date": "23\u65E5\u661F\u671F\u4E94",
+                "sunrise": "06:16",
+                "high": "\u9AD8\u6E29 18.0\u2103",
+                "low": "\u4F4E\u6E29 5.0\u2103",
+                "sunset": "18:28",
+                "aqi": 118,
+                "fx": "\u65E0\u6301\u7EED\u98CE\u5411",
+                "fl": "<3\u7EA7",
+                "type": "\u591A\u4E91",
+                "notice": "\u9634\u6674\u4E4B\u95F4\uFF0C\u8C28\u9632\u7D2B\u5916\u7EBF\u4FB5\u6270"
+            },
+            {
+                "date": "24\u65E5\u661F\u671F\u516D",
+                "sunrise": "06:14",
+                "high": "\u9AD8\u6E29 21.0\u2103",
+                "low": "\u4F4E\u6E29 7.0\u2103",
+                "sunset": "18:29",
+                "aqi": 52,
+                "fx": "\u897F\u5357\u98CE",
+                "fl": "<3\u7EA7",
+                "type": "\u6674",
+                "notice": "\u613F\u4F60\u62E5\u6709\u6BD4\u9633\u5149\u660E\u5A9A\u7684\u5FC3\u60C5"
+            },
+            {
+                "date": "25\u65E5\u661F\u671F\u65E5",
+                "sunrise": "06:13",
+                "high": "\u9AD8\u6E29 22.0\u2103",
+                "low": "\u4F4E\u6E29 7.0\u2103",
+                "sunset": "18:30",
+                "aqi": 71,
+                "fx": "\u897F\u5357\u98CE",
+                "fl": "<3\u7EA7",
+                "type": "\u6674",
+                "notice": "\u613F\u4F60\u62E5\u6709\u6BD4\u9633\u5149\u660E\u5A9A\u7684\u5FC3\u60C5"
+            },
+            {
+                "date": "26\u65E5\u661F\u671F\u4E00",
+                "sunrise": "06:11",
+                "high": "\u9AD8\u6E29 21.0\u2103",
+                "low": "\u4F4E\u6E29 8.0\u2103",
+                "sunset": "18:31",
+                "aqi": 97,
+                "fx": "\u897F\u5357\u98CE",
+                "fl": "<3\u7EA7",
+                "type": "\u591A\u4E91",
+                "notice": "\u9634\u6674\u4E4B\u95F4\uFF0C\u8C28\u9632\u7D2B\u5916\u7EBF\u4FB5\u6270"
+            }
+        ]
+    }
+}`),w=x(),j="calc(100vh - 130px)",D=x(!1),r=K({modalAssign:"this",typeSuffix:"0",firstLetter:"default"}),U=x(["0","-1"]),J=e=>{r.typeSuffix=e},R=(e,t)=>{const E=e?U.value.filter(q(e)):U.value;t(E)},I=x(["this","state","data"]),G=e=>{r.modalAssign=e},H=(e,t)=>{const E=e?I.value.filter(q(e)):I.value;t(E)},q=e=>t=>t.toLowerCase().indexOf(e.toLowerCase())===0,P=()=>{if(!b.value)return;let e=JSON.parse(JSON.stringify(b.value));try{e=JSON.parse(e)}catch{return k.info("JSON\u683C\u5F0F\u4E0D\u6B63\u786E")}w.value=Q(e,"",!1,!1,"","")},Q=(e,t,E,f,g,v)=>{let _="";const A=W(e);let u="";function $(n,p,y,L,a,d){if(n&&typeof n=="object")if(Array.isArray(n))S(n,p,y,L,a,d);else{d?d=d+"."+p:d=p;for(let F=Object.keys(n),m=0,B=F.length;m<B;m++){const o=F[m],h=n[o];let l=!1;const c=m===0;l=m===B-1,$(h,o,c,l,p,d)}}else S(n,p,y,L,a,d)}function S(n,p,y,L,a,d){let F="",m="",B=[];const o=d==null?void 0:d.split(".");switch(r.firstLetter){case"uppercase":F=p.substring(0,1).toLocaleUpperCase()+p.substring(1),a&&(m=a.substring(0,1).toLocaleUpperCase()+a.substring(1)),o&&o.length>0&&o.forEach(l=>{B.push(l.substring(0,1).toLocaleUpperCase()+l.substring(1))});break;case"lowercase":F=p.substring(0,1).toLocaleLowerCase()+p.substring(1),m=a.substring(0,1).toLocaleLowerCase()+a.substring(1),o&&o.length>0&&o.forEach(l=>{B.push(l.substring(0,1).toLocaleLowerCase()+l.substring(1))});break;default:F=p,m=a,B=o;break}let h='""';if(typeof n=="object"&&Array.isArray(n)?h="[]":typeof n=="string"||n==="null"?h='""':typeof n=="number"&&(h=r.typeSuffix),y&&a&&(o&&o.length>1?(u.length===0&&(B.forEach((l,c)=>{c<B.length-1&&(c===0?u=`${r.modalAssign}.${l} = {
+`:u=`${u}${l}: {
+`)}),_="};"),u=`${u}${m}: {
+`,u=`${u}${F}:${h},
+`):(u=`${u}${r.modalAssign}.${m} = {
+`,u=`${u}${F}:${h},
+`)),y&&!a&&(u=`${r.modalAssign}.${F} = ${h};
+`),!y&&a&&(u=u+`${F}: ${h}`,u=`${u},
+`),!y&&!a&&(u=u+`${r.modalAssign}.${F} = ${h};
+`),L&&a){if(o&&o.length>=2){const l=A.find(c=>c.key===a);if(o&&o.length>=3){const c=A.find(C=>C.key===l.key);if(c&&c.parentKey){const C=A.filter(O=>O.parentKey===c.parentKey);if(C.length-1===c.index)u=`${u}},
+},`;else for(let O=0;O<C.length-c.index;O++)u=`${u}},
+`;return}}else if(l&&l.parentKey){A.filter(C=>C.parentKey===l.parentKey).length-1===l.index?u=`${u}},
+};`:u=`${u}},
+`;return}u=`${u}},
+};`;return}u=`${u}};
+`}}return $(e,t,E,f,g,v),u=pu.exports.js_beautify(u+_),u},W=e=>{const t=[];function E(f,g,v,_){f&&typeof f=="object"&&!Array.isArray(f)?(g&&t.push({key:g,parentKey:v,index:_}),v=g,Object.keys(f).forEach((A,u)=>{E(f[A],A,v,u)})):t.push({key:g,parentKey:v,index:_})}return E(e,"","",0),t},X=()=>{if(!!b.value)try{const e=JSON.parse(b.value);b.value="",tu(()=>{b.value=JSON.stringify(e,null,4)})}catch(e){return k.info("JSON\u683C\u5F0F\u4E0D\u6B63\u786E\uFF1A",e==null?void 0:e.message)}},Y=async()=>{if(!w.value)return k.info("\u6CA1\u6709\u5185\u5BB9");try{await M(w.value),D.value=!0,setTimeout(()=>{D.value=!1},1e3)}catch{D.value=!1,k.error("\u590D\u5236\u5931\u8D25")}};return(e,t)=>{const E=lu,f=ou,g=au,v=iu,_=ru,A=cu,u=uu,$=fu,S=Eu;return nu(),eu("div",null,[V("div",du,[s(A,{"label-width":"40px",inline:!0,size:"small",class:"no-bottom"},{default:i(()=>[s(f,{label:"assign\u5B9A\u4E49","label-width":"80px"},{default:i(()=>[s(E,{modelValue:r.modalAssign,"onUpdate:modelValue":t[0]||(t[0]=n=>r.modalAssign=n),size:"small","fetch-suggestions":H,onSelect:G,clearable:"",class:"w-100",placeholder:"\u8F93\u5165\u6216\u9009\u62E9"},{default:i(({item:n})=>[V("div",Fu,T(n),1)]),_:1},8,["modelValue"])]),_:1}),s(f,{label:"\u9ED8\u8BA4number","label-width":"90px"},{default:i(()=>[s(E,{modelValue:r.typeSuffix,"onUpdate:modelValue":t[1]||(t[1]=n=>r.typeSuffix=n),size:"small","fetch-suggestions":R,onSelect:J,clearable:"",class:"w-100",placeholder:"\u8F93\u5165\u6216\u9009\u62E9"},{default:i(({item:n})=>[V("div",mu,T(n),1)]),_:1},8,["modelValue"])]),_:1}),s(f,{label:"\u9996\u5B57\u6BCD","label-width":"50px"},{default:i(()=>[s(v,{modelValue:r.firstLetter,"onUpdate:modelValue":t[2]||(t[2]=n=>r.firstLetter=n),class:"w-100"},{default:i(()=>[s(g,{label:"\u9ED8\u8BA4",value:"default"}),s(g,{label:"\u5927\u5199",value:"uppercase"}),s(g,{label:"\u5C0F\u5199",value:"lowercase"})]),_:1},8,["modelValue"])]),_:1}),s(f,null,{default:i(()=>[s(_,{type:"default",onClick:X},{default:i(()=>[gu]),_:1}),s(_,{type:"primary",onClick:P},{default:i(()=>[hu]),_:1})]),_:1})]),_:1}),V("div",_u,[s(u,{visible:D.value,"show-arrow":!1,effect:"light",placement:"left",content:"\u590D\u5236\u6210\u529F"},{default:i(()=>[s(_,{type:"default",size:"small",onClick:Y},{default:i(()=>[Au]),_:1})]),_:1},8,["visible"])])]),s(S,null,{default:i(()=>[s($,{span:12},{default:i(()=>[s(z,{class:"r-b",value:b.value,"onUpdate:value":t[3]||(t[3]=n=>b.value=n),height:j,language:"json",placeholder:"\u8BF7\u8F93\u5165JSON"},null,8,["value"])]),_:1}),s($,{span:12},{default:i(()=>[s(z,{value:w.value,"onUpdate:value":t[4]||(t[4]=n=>w.value=n),language:"typescript",height:j},null,8,["value"])]),_:1})]),_:1})])}}});export{Cu as default};
