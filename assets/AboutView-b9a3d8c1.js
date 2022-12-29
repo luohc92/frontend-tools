c909ea9d04cefc3796eff0a6fd6948f92e7eafbd
luohc92 <luohc92@gmail.com>
2022-12-29 14:41:48 +0800

    new

--- assets/AboutView-b9a3d8c1.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as o,o as s,a as t}from"./index-6b00a475.js";const _={},c={class:"about"},n=t("h1",null,"This is an about page",-1),a=[n];function r(i,u){return s(),o("div",c,a)}const d=e(_,[["render",r],["__file","C:/Users/luohc/Documents/work/frontend-tools-box/src/views/AboutView.vue"]]);export{d as default};
